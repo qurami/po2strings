a5cc2ef3251eb20038104fae8bd28815f8a8e602
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2017-10-10 19:27:06 +0200

    Add custom object_hook method for json conversion
    cfr. https://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-of-unicode-from-json

--- po2strings/importers/arb.py
+++ po2strings/importers/arb.py
@@ -167,4 +167,25 @@ class ArbImporter:
         # save the arb_file_as_dict as self.destination_file
         with open(self.destination_file, 'w+') as sf:
             json.dump(arb_file_as_dict, sf)
-            sf.close()
+            sf.close()def _byteify(data, ignore_dicts = False):
+    # if this is a unicode string, return its string representation
+    if isinstance(data, unicode):
+        return data.encode('utf-8')
+    # if this is a list of values, return list of byteified values
+    if isinstance(data, list):
+        return [ _byteify(item, ignore_dicts=True) for item in data ]
+    # if this is a dictionary, return dictionary of byteified keys and values
+    # but only if we haven't already byteified it
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+            for key, value in data.iteritems()
+        }
+    # if it's anything else, return it in its original form
+    return data
+
+def json_load_byteified(file_handle):
+    return _byteify(
+        json.load(file_handle, object_hook=_byteify),
+        ignore_dicts=True
+    )
